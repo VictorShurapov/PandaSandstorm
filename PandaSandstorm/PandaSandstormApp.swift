4d94c988ede6fc86bddf465a47d64c3866b951b8
Viktor <vshurapov@gmail.com>
2024-02-15 22:53:12 +0200

    Initial Commit

---
+++ PandaSandstorm/PandaSandstormApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaSandstormApp.swift
+//  PandaSandstorm
+//
+//  Created by Yaro4ka on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PandaSandstormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
